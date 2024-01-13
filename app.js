a6acfaa415ce1cbf1933246e04aeb4c07680beb4
soham sarkar <sohamsarkar59@gmail.com>
2024-01-13 19:12:22 +0530

    added cors

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ import express from "express";
 import session from "express-session";
 import passport from "passport";
 import errorMiddleware from "./middlewares/error.js";
+import cors from "cors";
 
 import { config } from "dotenv";
 config({ path: "./config/config.env" });
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    optionsSuccessStatus: 200,
+    exposedHeaders: ["Set-Cookie"],
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Access-Control-Allow-Origin",
+      "Content-Type",
+      "Authorization",
+      "Set-Cookie",
+    ],
+  })
+);
+
+app.use(express.json());
 
 import intializePassport from "./config/passport.js";
 intializePassport(passport);
@@ -27,14 +46,10 @@ app.get("/", (req, res) => {
 });
 
 import authRouter from "./routes/auth.js";
-import appRouter from "./routes/app.js";
 import backgroundRouter from "./routes/background.js";
-import widgetRouter from "./routes/widgets.js";
 
 app.use("/auth", authRouter);
-app.use("/app", appRouter);
-app.use("/background", backgroundRouter);
-app.use("/widgets", widgetRouter);
+app.use("/api/v1/background", backgroundRouter);
 
 export default app;
 
