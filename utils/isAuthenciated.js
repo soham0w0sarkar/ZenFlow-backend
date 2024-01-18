a1889a847c3c0d7c555733f6b01c38be5edd93f2
soham sarkar <sohamsarkar59@gmail.com>
2024-01-18 18:24:31 +0530

    corrected aaccording to recent session managemnet

--- utils/isAuthenciated.js
+++ utils/isAuthenciated.js
@@ -1,6 +1,5 @@
 export const isAuthenciated = (req, res, next) => {
-  console.log(req.isAuthenticated());
-  if (req.isAuthenticated()) {
+  if (req.session.user) {
     next();
   } else {
     res.status(401).json({
