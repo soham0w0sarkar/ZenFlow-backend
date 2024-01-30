fe10e65a4591ced6e3b80a11c6cb3f25368ca616
soham sarkar <sohamsarkar59@gmail.com>
2024-01-31 05:09:11 +0530

    made a middleware to set oauth token

---
+++ middlewares/setOauthCredentials.js
@@ -0,0 +1,17 @@
+import { oauth2Client, refreshAccessToken } from '../config/oauth.js';
+import { catchAsyncError } from './catchAsyncError.js';
+
+const setOauthCredentials = catchAsyncError((req, res, next) => {
+	if (req.session.access_token_expiration < new Date().getTime()) {
+		refreshAccessToken(req.session.user.refresh_token);
+	}
+
+	oauth2Client.setCredentials({
+		access_token: req.session.user.access_token,
+		refresh_token: req.session.user.refresh_token
+	});
+
+	next();
+});
+
+export default setOauthCredentials;
