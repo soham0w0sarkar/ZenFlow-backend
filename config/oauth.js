42d72a9481789d4b8ba202e69c04a01f03680b15
soham sarkar <sohamsarkar59@gmail.com>
2024-01-31 05:07:57 +0530

    added refreshAccessToken to refresh the access token

--- config/oauth.js
+++ config/oauth.js
@@ -5,3 +5,18 @@ export let oauth2Client;
 export const initializeOAuthClient = () => {
 	oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, 'http://localhost:9090/api/v1/auth/google/callback');
 };
+
+export const refreshAccessToken = async (refreshToken) => {
+	oauth2Client.setCredentials({
+		refresh_token: req.session.user.refresh_token
+	  });
+	  
+	  oauth2Client.refreshAccessToken(function(err, tokens) {
+		if (err) {
+		  console.error('Error refreshing access token', err);
+		  return;
+		}
+
+		req.session.user.access_token = tokens.access_token;
+	});
+};
