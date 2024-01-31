df887187ffb8b276b38a927c97f12aed1b738c74
soham sarkar <sohamsarkar59@gmail.com>
2024-02-01 04:49:05 +0530

    added getAllMails route to widgetRouter

--- routes/widgets.js
+++ routes/widgets.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createEvent, getAllEvents, getJokes, getWeather } from '../controllers/widgets.js';
+import { createEvent, getAllEvents, getAllMails, getJokes, getWeather } from '../controllers/widgets.js';
 import { isAuthenciated } from '../utils/isAuthenciated.js';
 import setOauthCredentials from '../middlewares/setOauthCredentials.js';
 
@@ -12,5 +12,6 @@ widgetRouter
 	.route('/calendar')
 	.get(isAuthenciated, setOauthCredentials, getAllEvents)
 	.post(isAuthenciated, setOauthCredentials, createEvent);
+widgetRouter.get('/allMails', isAuthenciated, setOauthCredentials, getAllMails)
 
 export default widgetRouter;
