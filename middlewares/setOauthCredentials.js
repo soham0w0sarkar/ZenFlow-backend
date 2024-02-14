639d3b2212db24fa463321b55932fc8488587358
soham sarkar <sohamsarkar59@gmail.com>
2024-02-14 17:31:38 +0530

    Fix access token refresh logic

--- middlewares/setOauthCredentials.js
+++ middlewares/setOauthCredentials.js
@@ -2,10 +2,9 @@ import { oauth2Client, refreshAccessToken } from '../config/oauth.js';
 import { catchAsyncError } from './catchAsyncError.js';
 
 const setOauthCredentials = catchAsyncError((req, res, next) => {
-	// if (req.session.access_token_expiration <= new Date().getTime()) {
-	const resToken = refreshAccessToken(req.session.user.refresh_token, req);
-	
-	// }
+	if (req.session.access_token_expiration <= new Date().getTime()) {
+		refreshAccessToken(req.session.user.refresh_token, req);
+	}
 
 	oauth2Client.setCredentials({
 		access_token: req.session.user.access_token,
