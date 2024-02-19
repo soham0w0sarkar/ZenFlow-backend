8e0d0cb41d3337b9d1dbc4c95a052b057334bbc9
soham sarkar <sohamsarkar59@gmail.com>
2024-02-19 14:23:36 +0530

    added setCredentials route to authRouter

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { googleLogin, googleCallback, status, logout } from '../controllers/auth.js';
+import { googleLogin, googleCallback, status, logout, setCredentials } from '../controllers/auth.js';
 import { isAuthenciated } from '../utils/isAuthenciated.js';
 
 const authRouter = express.Router();
 
 authRouter.get('/googleLogin', googleLogin);
 authRouter.get('/google/callback', googleCallback);
+authRouter.post('/setCredentials/:id', setCredentials);
 authRouter.get('/status', isAuthenciated, status);
 authRouter.get('/logout', logout);
 
